4b0e96bd445bb1e3f5e09f74752c838da4430fbe
Claire Herdeman <34634350+cherdeman@users.noreply.github.com>
2018-08-15 16:29:05 -0400

    update threshold variable

--- infrastructure/ignore.sh
+++ infrastructure/ignore.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Initialize variables
+MB=100
 counter=0
 
 # Check for existence of .gitignore
@@ -12,6 +14,7 @@ do
 done
 
 # If no .gitignore create and add header, if exists overwrite with header
+# Note that this section can be modified to append rather than overwrite if the .gitignore already exists
 if [ $counter = 0 ]; 
 then touch .gitignore
 	echo "# First Line" > .gitignore
@@ -19,5 +22,5 @@ else
 	echo "# First Line" > .gitignore
 fi
 
-# Append all files where size is > 100M
-find . -size +100M | sed 's/^.//' | cat >> .gitignore
+# Append all files where size is > the threshold (in megabytes) set in the MB variable
+find . -size +${MB}M | sed 's/^.//' | cat >> .gitignore
